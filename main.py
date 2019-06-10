5e627350dc9e6d61d73d48d6ad07dabf179b528b
Prinu Dickson <prinu.dickson@nl.pwc.com>
2019-06-10 11:00:05 +0200

    flask app with two pages
    flask app with two pages

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+#Note:
+#Please follow the below link to understand the app better.
+#https://www.freecodecamp.org/news/how-to-build-a-web-application-using-flask-and-deploy-it-to-the-cloud-3551c985e492/
+
+#to be done
+# need to include sample viz
+# https://www.fullstackpython.com/blog/responsive-bar-charts-bokeh-flask-python-3.html
+# need to include a connection to a DB 
+# need to figure out the tutorial
+
 from flask import Flask, render_template    
 
 app = Flask(__name__)
@@ -6,9 +16,9 @@ app = Flask(__name__)
 def home():
    return render_template("home.html")
 
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ other_code.py
@@ -0,0 +1,7 @@
+#pip install virtualenv
+
+#virtualenv “flask_ini_env”
+
+#flask_ini_env\Scripts\activate
+
+#pip install flask
