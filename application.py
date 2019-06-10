5aabe7b505311e828d6c5c613c57d42e0482725a
Prinu Dickson <prinu.dickson@nl.pwc.com>
2019-06-10 23:44:04 +0200

    testing_auth
    testing_auth

--- application.py
+++ application.py
@@ -10,7 +10,36 @@
 
 from flask import Flask, render_template    
 
+from flask_azure_oauth import FlaskAzureOauth
+
 app = Flask(__name__)
+
+app.config['AZURE_OAUTH_TENANCY'] = 'xxx'
+app.config['AZURE_OAUTH_APPLICATION_ID'] = 'xxx'
+app.config['AZURE_OAUTH_CLIENT_APPLICATION_IDS'] = ['xxx']
+
+auth = FlaskAzureOauth()
+auth.init_app(app=app)
+
+@app.route('/unprotected')
+def unprotected():
+    return 'hello world'
+
+
+@app.route('/protected')
+@auth()
+def protected():
+    return 'hello authenticated entity'
+
+@app.route('/protected-with-single-scope')
+@auth('required-scope')
+def protected_with_scope():
+    return 'hello authenticated and authorised entity'
+
+@app.route('/protected-with-multiple-scopes')
+@auth('required-scope1 required-scope2')
+def protected_with_multiple_scopes():
+    return 'hello authenticated and authorised entity'
 
 @app.route("/")
 def home():
@@ -22,3 +51,8 @@ def about():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
