9a2c2b0beba7efcdef7b24ccba81d805df2fdf29
Prinu Dickson <prinu.dickson@nl.pwc.com>
2019-06-10 23:47:20 +0200

    revert back without auth
    revert back without auth

--- application.py
+++ application.py
@@ -10,36 +10,7 @@
 
 from flask import Flask, render_template    
 
-from flask_azure_oauth import FlaskAzureOauth
-
 app = Flask(__name__)
-
-app.config['AZURE_OAUTH_TENANCY'] = 'xxx'
-app.config['AZURE_OAUTH_APPLICATION_ID'] = 'xxx'
-app.config['AZURE_OAUTH_CLIENT_APPLICATION_IDS'] = ['xxx']
-
-auth = FlaskAzureOauth()
-auth.init_app(app=app)
-
-@app.route('/unprotected')
-def unprotected():
-    return 'hello world'
-
-
-@app.route('/protected')
-@auth()
-def protected():
-    return 'hello authenticated entity'
-
-@app.route('/protected-with-single-scope')
-@auth('required-scope')
-def protected_with_scope():
-    return 'hello authenticated and authorised entity'
-
-@app.route('/protected-with-multiple-scopes')
-@auth('required-scope1 required-scope2')
-def protected_with_multiple_scopes():
-    return 'hello authenticated and authorised entity'
 
 @app.route("/")
 def home():
